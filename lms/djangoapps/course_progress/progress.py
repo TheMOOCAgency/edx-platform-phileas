4492258ed6239f3873655a46c88f96c6d05e7f1e
naresh.makwana <naresh.makwana@drcsystems.com>
2016-06-22 14:42:05 +0530

    [IMPROVE] Include html view count and problem hints in course progress calculation.

--- lms/djangoapps/course_progress/progress.py
+++ lms/djangoapps/course_progress/progress.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from opaque_keys.edx.locations import SlashSeparatedCourseKey
 from courseware.courses import get_course_by_id
 from courseware.models import StudentModule
+from course_progress.models import StudentHistory
 
 
 @login_required
@@ -49,24 +50,16 @@ def get_course_progress(request):
                                 if not unit.visible_to_staff_only and component.start < timezone.now():
                                     if component.category in include_items:
                                             component_count += 1
-
-                                            try:
-                                                history = StudentModule.objects.get(module_state_key=component.location,
-                                                    student_id=student_id, course_id=course_key, module_type=component.category)
-                                            except:
-                                                history = None
-
-                                            if history:
-                                                if component.category == 'problem' and json.loads(history.state).get('attempts', 0):
-                                                    component_attempted +=1
-                                                elif component.category == 'video' and is_played(json.loads(history.state).get('saved_video_position', '00:00:00')):
-                                                    component_attempted +=1
-
-                            unit_progress = round(component_attempted * 100.00 / component_count, 2)
+                                            component_attempted += is_attempted(student_id, course_key, component)
+                            unit_progress = 0.0
+                            if component_count:
+                                unit_progress = round(component_attempted * 100.00 / component_count, 2)
                             if unit_progress == 100:
                                 unit_attempted += 1
                             units.update({str(unit.display_name_with_default): unit_progress})
-                    sub_section_progress = round(unit_attempted * 100.00 / len(units.keys()), 2)
+                    sub_section_progress = 0.0
+                    if units:
+                        sub_section_progress = round(unit_attempted * 100.00 / len(units.keys()), 2)
                     sub_sections.update({
                         str(sub_section.display_name_with_default): {
                             'sub_section_progress': sub_section_progress,
@@ -74,7 +67,9 @@ def get_course_progress(request):
                         }
                     })
             sub_section_progress_sum = sum([sub_section_dict.get('sub_section_progress', 0.0) for sub_section_dict in sub_sections.values()])
-            section_progress = round(sub_section_progress_sum * 1.00 / len(sub_sections.keys()), 2)
+            section_progress = 0.0
+            if sub_sections:
+                section_progress = round(sub_section_progress_sum * 1.00 / len(sub_sections.keys()), 2)
             sections.update({
                 str(section.display_name_with_default): {
                     'section_progress': section_progress,
@@ -83,7 +78,9 @@ def get_course_progress(request):
             })
 
     section_progress_sum = sum([section_dict.get('section_progress', 0.0) for section_dict in sections.values()])
-    course_progress = round(section_progress_sum * 1.00 / len(sections.keys()), 2)
+    course_progress = 0.0
+    if sections:
+        course_progress = round(section_progress_sum * 1.00 / len(sections.keys()), 2)
     progress.update({
         'course_progress': course_progress,
         'sections': sections
@@ -93,3 +90,37 @@ def get_course_progress(request):
 
 def is_played(time_str):
     return sum(map(int, time_str.split(':'))) > 0
+
+def is_attempted(student_id, course_key, component):
+    attempted = 0
+    state = get_component_state(component.location, student_id, course_key, component.category)
+
+    if component.category == 'problem':
+        if state.has_key('attempts'):
+            attempted = 1
+        elif get_component_done(component.location, student_id, course_key, component.category):
+            attempted = 1
+    elif component.category == 'video' and is_played(state.get('saved_video_position', '00:00:00')):
+        attempted = 1
+    elif component.category == 'html' and get_component_done(component.location, student_id, course_key, component.category):
+        attempted = 1
+
+    return attempted
+
+def get_component_state(module_state_key, student_id, course_key, module_type):
+    try:
+        history = StudentModule.objects.get(module_state_key=module_state_key,
+            student_id=student_id, course_id=course_key, module_type=module_type)
+    except:
+        history = None
+    
+    return json.loads(history.state) if history and history.state else {}
+
+def get_component_done(module_state_key, student_id, course_key, module_type):
+    try:
+        history = StudentHistory.objects.get(module_state_key=module_state_key,
+            student_id=student_id, course_id=course_key, module_type=module_type)
+    except:
+        history = None
+
+    return history and history.done == 'f'
