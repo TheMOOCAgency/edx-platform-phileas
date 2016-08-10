a8a0abda61b25c8006ff8f26a86ae01ff5fe28b5
naresh.makwana <naresh.makwana@drcsystems.com>
2016-08-10 12:44:17 +0530

    [ADD] Links on welcome page to redirect at last accessed section.

--- lms/djangoapps/course_welcome/helpers.py
+++ lms/djangoapps/course_welcome/helpers.py
@@ -1,18 +1,22 @@
 """
 Course info helpers
 """
-from edxmako.shortcuts import render_to_string
-from xmodule.modulestore.django import modulestore
-from courseware.module_render import get_module_for_descriptor, _add_timed_exam_info
-from courseware.model_data import FieldDataCache, ScoresClient
-from util import milestones_helpers
-from openedx.core.lib.gating import api as gating_api
-from courseware.entrance_exams import user_must_complete_entrance_exam
-from util.model_utils import slugify
 from django.core.context_processors import csrf
 from django.contrib.auth.models import User
-from util.db import outer_atomic
+
+from edxmako.shortcuts import render_to_string
+from xmodule.modulestore.django import modulestore
+from openedx.core.lib.gating import api as gating_api
+
+from courseware.module_render import get_module_for_descriptor, _add_timed_exam_info
+from courseware.model_data import FieldDataCache, ScoresClient
+from courseware.entrance_exams import user_must_complete_entrance_exam
+from courseware.views.views import get_current_child
 from courseware import grades
+
+from util import milestones_helpers
+from util.db import outer_atomic
+from util.model_utils import slugify
 
 
 def prepare_chapters_with_grade(request, course):
@@ -48,6 +52,12 @@ def prepare_chapters_with_grade(request, course):
     nonzero_cutoffs = [cutoff for cutoff in course.grade_cutoffs.values() if cutoff > 0]
     success_cutoff = min(nonzero_cutoffs) * 100 if nonzero_cutoffs else 0
 
+    # get the courseware position where left off
+    active_section = None
+    if course.has_children_at_depth(2):
+        active_chapter = get_last_accessed_chapter(course)
+        active_section = get_last_accessed_section(active_chapter)
+
     chapters = []
     section_index = 0
     for chapter in courseware_summary:
@@ -61,10 +71,13 @@ def prepare_chapters_with_grade(request, course):
                 sections.append({
                     'section_index': section_index,
                     'display_name': section['display_name'],
-                    'passed': success_cutoff and percentage >= success_cutoff
+                    'url_name': section['url_name'],
+                    'passed': success_cutoff and percentage >= success_cutoff,
+                    'paused': active_section and (section['url_name'] == active_section.url_name)
                 })
             chapters.append({
                 'display_name': chapter['display_name'],
+                'url_name': chapter['url_name'],
                 'sections': sections
             })
 
@@ -100,3 +113,18 @@ def get_final_score(request, course):
     final_grade = grade_summary.get('percent', 0)
 
     return int(final_grade * 100)
+
+def get_last_accessed_chapter(course):
+    """
+    It returns the last accessed chapter in
+    the course.
+    """
+    return get_current_child(course, min_depth=1, requested_child=None)
+
+def get_last_accessed_section(chapter):
+    """
+    It returns the last accessed section in
+    the chapter.
+    """
+    return get_current_child(chapter, min_depth=None, requested_child=None)
+
