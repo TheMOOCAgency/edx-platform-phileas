b1061ccf4b4def0791dd514f062bd1c0f16b5eb7
naresh.makwana <naresh.makwana@drcsystems.com>
2016-10-11 15:10:33 +0530

    [LMS: COMPLETION TRACKING] Fix: "Issue when course imported"

--- lms/djangoapps/course_progress/helpers.py
+++ lms/djangoapps/course_progress/helpers.py
@@ -17,6 +17,14 @@ from student.models import CourseEnrollment
 from course_progress.models import StudentCourseProgress
 
 
+# Valid components for tracking
+VALID_COMPONENTS = ['video', 'problem', 'html', 'openassessment', 'lti']
+
+VALID_BLOCKS = [
+    'course', 'chapter', 'sequential', 'vertical'
+] + VALID_COMPONENTS
+
+
 def inject_course_progress_into_context(context, request, course_key):
     """
     Set params to view context based on course_key and user
@@ -122,10 +130,10 @@ def get_component_state(student_id, course_key, instance):
 
 
 def get_default_course_progress(blocks, root):
-    ordered_blocks = OrderedDict()
+    default_course_progress = {}
     if blocks:
-        traverse_tree(root, blocks, ordered_blocks)
-    return ordered_blocks
+        default_course_progress = fix_block_ids(root, blocks)
+    return default_course_progress
 
 def traverse_tree(block, unordered_structure, ordered_blocks, parent=None):
     """
@@ -144,18 +152,11 @@ def traverse_tree(block, unordered_structure, ordered_blocks, parent=None):
     ordered_blocks[block] = cur_block
 
     # Allow only tracking related elements
-    valid_components = [
-        'course', 'chapter', 'sequential', 'vertical',
-        'video', 'problem', 'html', 'openassessment', 'lti'
-    ]
     for child_node in cur_block.get('children', []):
-        if unordered_structure[child_node]['type'] in valid_components:
+        if unordered_structure[child_node]['type'] in VALID_BLOCKS:
             traverse_tree(child_node, unordered_structure, ordered_blocks, parent=block)
         else:
-            original_children = ordered_blocks[cur_block['id']]['children']
-            original_children.remove(unordered_structure[child_node]['id'])
-            ordered_blocks[cur_block['id']]['children'] = original_children
-
+            ordered_blocks[block]['children'].remove(child_node)
 
 def set_initial_progress(request, course_key):
     course_usage_key = modulestore().make_course_usage_key(course_key)
@@ -195,3 +196,83 @@ def get_overall_progress(student_id, course_key):
         pass
 
     return overall_progress
+
+def preserve_block_usage_id(block):
+    # patch: block ID differs at API and LMS, if course imported
+    original_block_id = block
+    usage_key_splitted = block.split("+branch@draft")
+    if len(usage_key_splitted) > 1:
+        block_id_type_part = usage_key_splitted[1].split("+type@")[1]
+        original_block_id = usage_key_splitted[0] + "+type@" + block_id_type_part
+
+    return original_block_id
+
+def fix_block_ids(course_key, blocks):
+    modified_course_struct = {}
+    modified_course_block = {}
+    modified_course_block_children = []
+
+    course_block = blocks[course_key]
+    for chapter_id in course_block.get('children', []):
+        modified_chapter_id = preserve_block_usage_id(chapter_id)
+        modified_chapter_block = {}
+        modified_chapter_block_children = []
+
+        chapter_block = blocks[chapter_id]
+        for sequential_id in chapter_block.get('children', []):
+            modified_sequential_id = preserve_block_usage_id(sequential_id)
+            modified_chapter_block = {}
+            modified_sequential_block_children = []
+
+            sequential_block = blocks[sequential_id]
+            for vertical_id in sequential_block.get('children', []):
+                modified_vertical_id = preserve_block_usage_id(vertical_id)
+                modified_vertical_block = {}
+                modified_vertical_block_children = []
+
+                vertical_block = blocks[vertical_id]
+                for component_id in vertical_block.get('children', []):
+                    if blocks[component_id]['type'] in VALID_COMPONENTS:
+                        modified_component_id = preserve_block_usage_id(component_id)
+                        component_block = blocks[component_id]
+                        component_block['id'] = modified_component_id
+                        component_block['parent'] = modified_vertical_id
+                        component_block['progress'] = 0
+                        modified_course_struct.update({
+                            modified_component_id: component_block
+                        })
+                        modified_vertical_block_children += [modified_component_id]
+
+                vertical_block['id'] = modified_vertical_id
+                vertical_block['parent'] = modified_sequential_id
+                vertical_block['children'] = modified_vertical_block_children
+                vertical_block['progress'] = 0
+                modified_course_struct.update({
+                    modified_vertical_id: vertical_block
+                })
+                modified_sequential_block_children += [modified_vertical_id]
+
+            sequential_block['id'] = modified_sequential_id
+            sequential_block['parent'] = modified_chapter_id
+            sequential_block['children'] = modified_sequential_block_children
+            sequential_block['progress'] = 0
+            modified_course_struct.update({
+                modified_sequential_id: sequential_block
+            })
+            modified_chapter_block_children += [modified_sequential_id]
+
+        chapter_block['id'] = modified_chapter_id
+        chapter_block['parent'] = course_key
+        chapter_block['children'] = modified_chapter_block_children
+        chapter_block['progress'] = 0
+        modified_course_struct.update({
+            modified_chapter_id: chapter_block
+        })
+        modified_course_block_children += [modified_chapter_id]
+    course_block['children'] = modified_course_block_children
+    course_block['progress'] = 0
+    modified_course_struct.update({
+        course_key: course_block
+    })
+
+    return modified_course_struct
